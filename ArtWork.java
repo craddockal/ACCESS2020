e4863213b2807950da9332c8d725b69dd3d2a04f
Allyson Craddock <allyson.craddock@scsboe.org>
2021-01-06 08:15:51 -0600

    Create ArtWork.java

---
+++ ArtWork.java
@@ -0,0 +1,24 @@
+/**
+ * This program creates an ASCII Art stick figure.
+ *
+ * @author (Leon Lucious Jr.)
+ * @version (Sat, Jan 2)
+ */
+public class ArtWork
+{
+    public ArtWork()
+    {
+        //local variables
+        System.out.print("*       *****   *****   **    *\n");
+        System.out.print("*       *       *   *   * *   *\n");
+        System.out.print("*       *****   *   *   *  *  *\n");
+        System.out.print("*       *       *   *   *   * *\n");
+        System.out.print("*****   *****   *****   *    **\n");
+        System.out.print("\n");
+        System.out.print("   *******   *****     *         *     *\n");
+        System.out.print("      *      *       *   *      * *   * *\n");
+        System.out.print("      *      *****   *****     *   * *   *\n");
+        System.out.print("      *      *       *   *    *     *     *\n");
+        System.out.print("      *      *****   *   *   *             *\n");
+    }
+}
